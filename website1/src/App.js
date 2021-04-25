e15eefbd700bf68471a8fe0e1023ca1464ab0fc9
Austin Chan <achan113@ucmerced.edu>
2021-04-24 23:36:29 -0700

    Hello World fail

---
+++ website1/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import toDoList from './toDoList'
+
+export default function App() {
+  return (
+    <div>
+      <h1>
+        Hello World
+      </h1>
+    </div>
+    //<toDoList />
+  );
+}
---
+++ website1/src/toDoList.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function toDoList() {
+    return (
+        <div>
+            Hello World
+        </div>
+    );
+}
+
+export default toDoList
